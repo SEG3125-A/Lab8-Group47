6c8bb48b91a0341f9c8d4e567f72bb975d176e8b
aclar119 <aclar119@uottawa.ca>
2024-03-17 21:40:09 -0400

    Fixed navbar not filling width on firefox

--- src/app/workouts/page.jsx
+++ src/app/workouts/page.jsx
@@ -2,7 +2,7 @@
 export default function Workouts() {
   return (
     <div>
-      <div class="mainText" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+      <div className="mainText" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
         <h1>
           Workouts
         </h1>
