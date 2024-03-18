6d60dfcad2319ec1e54c7e3784864df5a5bb44e3
Justin <jbush023@uottawa.ca>
2024-03-17 21:39:07 -0400

    added workouts

--- src/app/workouts/page.jsx
+++ src/app/workouts/page.jsx
@@ -1,12 +1,66 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import styles from './styles.module.css';
+import { symlink } from 'fs';
+
 
 export default function Workouts() {
   return (
-    <div>
-      <div class="mainText" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-        <h1>
-          Workouts
-        </h1>
+    <div className={styles.pageBody}>
+      <div className={styles.mainText} >
+        <div> 
+          Workout History
+        </div>
+      </div>
+      <div className={styles.workout} >
+        <div className={styles.workoutInformation} >
+          <div className={styles.informationEntry} >
+            <div className={styles.informationEntryLabel} >
+              Date
+            </div>
+            <div className={styles.informationEntryValue} >
+              2024-03-10
+            </div>
+          </div>
+          <div className={styles.informationEntry} >
+            <div className={styles.informationEntryLabel} >
+              TIME (STARTED)
+            </div>
+            <div className={styles.informationEntryValue} >
+              3:00 PM
+            </div>
+          </div>
+          <div className={styles.informationEntry} >
+            <div className={styles.informationEntryLabel} >
+              EXERCISE TYPE
+            </div>
+            <div className={styles.informationEntryValue} >
+              Pushups
+            </div>
+          </div>
+          <div className={styles.informationEntry} >
+            <div className={styles.informationEntryLabel} >
+              SETS
+            </div>
+            <div className={styles.informationEntryValue} >
+              4
+            </div>
+          </div>
+          <div className={styles.informationEntry} >
+            <div className={styles.informationEntryLabel} >
+              REPS (Per Set)
+            </div>
+            <div className={styles.informationEntryValue} >
+              15
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.addWorkoutButton} >
+        <div className={styles.plusLogo} >
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
