7ea056543cd07bca667daec8ff4f4d41b615c04a
Saad Mazhar <smazh1@ocdsb.ca>
2024-03-23 22:47:41 -0400

    created basic log in and sign up pages

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,13 +1,68 @@
-
+'use client';
+import React, { useEffect, useState } from "react";
+import { usePathname, useRouter } from 'next/navigation';
+import './styles.css';
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [email, setEmail] = useState("");
+    const router = useRouter()
+    const handleSubmit = (event) => {
+
+      event.preventDefault();
+
+      if (password !== passwordConfirm) {
+         alert("Passwords do not match");
+         return;
+      }
+      else if (password.length == 0 || passwordConfirm.length == 0 || email.length == 0 || username.length == 0){
+        alert("One or more field(s) are empty");
+        return
+      }
+
+      console.log(
+        `Submitted username: ${username}, password: ${password}, email: ${email}`
+      );
+      router.push('/login');
+
+    };
+
+  const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+    };
+
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+
+    const handlePasswordConfirmChange = (event) => {
+      setPasswordConfirm(event.target.value);
+    };
+
     return (
-      <div>
-        <div className="mainText" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+      <div className="containerDiv">
+        <div>
           <h1>
-            Sign Up
+            Create an Account
           </h1>
+        </div>
+        <div className="signupForm">
+          <form className="signup-form" action="" method="post" onSubmit={handleSubmit} >
+            <input type="email" className="input" id="user_email" autoComplete="off" placeholder="Email" value={email} onChange={handleEmailChange} />
+            <input type="text" className="input" id="user_name" autoComplete="off" placeholder="Username" value={username} onChange={handleUsernameChange} />
+            <input type="password" className="input" id="user_pass" autoComplete="off" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+            <input type="password" className="input" id="user_pass_confirm" autoComplete="off" placeholder="Confirm Password" value={passwordConfirm} onChange={handlePasswordConfirmChange} />
+            <div className="buttonContainer">
+              <input type="submit" className="button" value="Sign Up" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     );
 }
   
