d4443ed676307abf63ebcbc90bf0be4ac0015ed9
aclar119 <aclar119@uottawa.ca>
2024-03-24 20:21:26 -0400

    Fixed "Goals" page and connected to data store

---
+++ src/app/goals/components/goal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./styles.module.css";
+import { Exercises } from "../../store";
+import StatDisplay from "../../components/stat_display/StatDisplay";
+
+
+export type GoalData = {
+    exerciseType: Exercises;
+    sets: number;
+    reps: number;
+}
+
+export type GoalProps = {
+    style?: React.CSSProperties;
+    data: GoalData;
+}
+
+export default function Goal(props: GoalProps) {
+    
+    return (
+        <div className={styles.goal}>
+
+            {/* Top Section - Title and Progress Ring */}
+            <div className={styles.visual}>
+                <div className={styles.exerciseType}>
+                    <h3>{props.data.exerciseType}</h3>
+                </div>
+                <div className={styles.goalRing} />
+            </div>
+
+            {/* Bottom Section - Stats */}
+            <div className={styles.goalInfo}>
+                <StatDisplay size="lg" data={{
+                    name: "Total Sets", 
+                    value: props.data.sets.toString(),
+                }}/>
+                <StatDisplay size="lg" data={{
+                    name: "Reps (Per Set)", 
+                    value: props.data.reps.toString(),
+                }}/>
+            </div>
+            
+            {/* <button className={styles.deleteButton} onClick={() => deleteSomething(index)}>Delete</button> */}
+        
+        </div>
+    );
+}
